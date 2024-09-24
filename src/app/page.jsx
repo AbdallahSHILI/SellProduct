17c20641dfb2a9ff3d72a752b90abcab21af988b
Abdallah <shiliabdallah1234@gmail.com>
2024-09-24 15:19:00 +0200

    fix home page element and their style

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,20 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Hero from "../../public/img/undraw_product_teardown_re_m1pc.svg";
 
 export default function Home() {
-  return <h1>Hello</h1>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.col}>
+        <h1 className={styles.title}>Your One-stop e-commerce destination !</h1>
+        <p className={styles.description}>
+          Descover a world of endless shopping{" "}
+        </p>
+        <button className={styles.button}>Shop Now</button>
+      </div>
+      <div className={styles.col}>
+        <Image src={Hero} alt="Hex" />
+      </div>
+    </div>
+  );
 }
